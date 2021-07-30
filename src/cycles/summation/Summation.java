613c532ba25058e62438961c2906759bd2241bc9
VillainRU <electro.dmitriy@gmail.com>
2021-07-30 14:25:36 +0400

    Решена задача на суммирование в цикле

---
+++ src/cycles/summation/Summation.java
@@ -0,0 +1,28 @@
+package cycles.summation;
+
+import java.util.Scanner;
+
+/*
+boolean isExit = false;
+while (!isExit)
+{
+   String s = console.nextLine();
+   isExit = s.equals("exit");
+}
+ */
+public class Summation {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        boolean isEnter = false;
+        Scanner console = new Scanner(System.in);
+        int sum = 0;
+        while (!isEnter) {
+            String x = console.nextLine();
+            isEnter = x.equals("ENTER");
+            if (!isEnter) {
+                sum += Integer.parseInt(x);
+            }
+        }
+        System.out.println(sum);
+    }
+}
